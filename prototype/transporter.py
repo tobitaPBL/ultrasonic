8253771e96fc9be1a15b06ad4054fbdb5248c5ff
chihiro hashimoto <chr@chrhsmt.com>
2016-10-29 17:17:23 +0900

    Add some files

--- prototype/transporter.py
+++ prototype/transporter.py
@@ -1,8 +1,8 @@
 from ultrasonic_network import *
-
 import sys
 
 class Transporter:
+  DEFAULT_CHARSET = "US-ASCII"
   def __init__(self):
     self.network = UltrasonicNetwork()
     self.header = {}
@@ -21,12 +21,20 @@ class Transporter:
   def attach_receive_callback(self, func):
     self.receive_callback = func
 
+  def parse_receive_data(self, data):
+    # header算出のためにデフォルト文字コードでデコード
+    # 改行2つがある位置
+    # "\n".encode(self.DEFAULT_CHARSET)[0]
+    # header = data.decode(data)
+    # header.split("\n")
+    pass
+
   def __parse_send_data(self, data):
     # FIXME: 文字以外のデータを処理する場合には修正
     if self.header["charset"] != None:
       charset = self.header["charset"]
     else:
-      charset = "US-ASCII"
+      charset = self.DEFAULT_CHARSET
     print("charset: %s" % charset)
     data = "charset=%s\n\n%s\n" % (charset, data)
     print("transport data body:\n%s" % data)
--- prototype/ultrasonic_network.py
+++ prototype/ultrasonic_network.py
@@ -1,17 +1,65 @@
-
+from transducer import *
 import sys
+import threading
 
 class UltrasonicNetwork:
   def __init__(self, version=1):
     self.version = version
 
+
   def send(self, data):
+    u"""バイナリデータ送信
+    @param data bytes or bytearray class data.
+    """
+
     data = bytearray(data)
     print("Decimalize: %s" % [int(d) for d in data])
     print("Binarize: %s" %  [format(int(d), 'b') for d in data])
     # ヘッダを含めて全てバイナリ化
-    # Physical Layerへ渡す
+    # transducerへ渡す
+    transducer.send(data)
     pass
 
+  def start_listening(self):
+    listen_thread = threading.Thread(target = self.listen)
+    listen_thread.start()
+
+  def stop_listening(self):
+    self.do_listen = False
+
+  def quit(self):
+    self.do_quit = True
+
+  def listen(self):
+    u"""データ受信待機
+    """
+    self.do_listen = True
+    while (True):
+      if self.do_quit == True:
+        break
+      if self.do_listen == False:
+        continue
+      # transducerからバイナリ
+      bdata = None
+      data = bytearray(bdata)
+      # ヘッダ取得
+      self.__parse_header(data)
+      # データ部取得
+      data_body = self.__parse_body(data)
+      self.receive_callback(data_body)
+
   def attach_receive_callback(self, func):
+    u"""データ受信時コールバック設定
+    コールバック関数の引数は bytearray class.
+    """
     self.receive_callback = func
+
+  def __parse_header(self, data):
+      # version
+      # データ長
+      pass
+
+  def __parse_body(self, data):
+      # data body
+      return data
+
