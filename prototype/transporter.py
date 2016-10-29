3343e31fa2765343059364702bd27f755ef9f7a9
chihiro hashimoto <chr@chrhsmt.com>
2016-10-29 15:36:36 +0900

    Change data structure from transporter

--- prototype/transporter.py
+++ prototype/transporter.py
@@ -10,7 +10,7 @@ class Transporter:
 
   def send(self, message):
     # data部をencodeする
-    self.__parse_data(message)
+    self.__parse_send_data(message)
 
     # データをNetwork層へ渡す
     self.network.send(self.bdata)
@@ -21,11 +21,13 @@ class Transporter:
   def attach_receive_callback(self, func):
     self.receive_callback = func
 
-  def __parse_data(self, data):
+  def __parse_send_data(self, data):
     # FIXME: 文字以外のデータを処理する場合には修正
     if self.header["charset"] != None:
       charset = self.header["charset"]
     else:
       charset = "US-ASCII"
     print("charset: %s" % charset)
+    data = "charset=%s\n\n%s\n" % (charset, data)
+    print("transport data body:\n%s" % data)
     self.bdata = data.encode(charset)
